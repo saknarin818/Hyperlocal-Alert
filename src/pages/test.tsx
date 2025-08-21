2f8866e95f9576404c317992e0536c6b343923db
saknarin818 <saknarin828@gmail.com>
2025-08-21 23:46:14 +0700

    test

---
+++ src/pages/test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestPage: React.FC = () => {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Test Page</h1>
+      <p>หน้านี้สำหรับทดสอบ component หรือฟีเจอร์ต่าง ๆ</p>
+    </div>
+  );
+};
+
+export default TestPage;
